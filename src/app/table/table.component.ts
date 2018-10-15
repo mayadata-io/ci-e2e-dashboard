c142caf9cfdfbe85fc23ee60277b61109079f452
Chandan Kumar <chandan.kr404@gmail.com>
2018-10-15 21:17:07 +0530

    fix api url
    Signed-off-by: Chandan Kumar <chandan.kr404@gmail.com>

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -22,7 +22,7 @@ export class TableComponent implements OnInit {
     if ((this.host.toString().indexOf("localhost") + 1) && this.host.toString().indexOf(":")) {
       this.restItemsUrl = "http://localhost:3000/";
     } else if (this.host == "openebs.ci") {
-        this.restItemsUrl = "http://openebs.ci/api/";
+        this.restItemsUrl = "https://openebs.ci/api/";
     } else {
       this.restItemsUrl = "https://staging.openebs.ci/api/";
     }
