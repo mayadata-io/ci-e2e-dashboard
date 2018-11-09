9bf7dc905d9ee3144035ce6649b9f18bac98c7e0
inyee786 <intakhab.cusat@gmail.com>
2018-11-09 15:32:55 +0530

    Show pipeline running status
    * Show pipeline running status
    
    - Show font Awesome spinner when the pipeline is running
    
    * Sidebar
    
    - Fix side bar for workloads option
    - Remove the collapsed class form dropdown menu
    
    Signed-off-by: inyee786 <intakhab.cusat@gmail.com>

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -31,7 +31,7 @@ export class TableComponent implements OnInit {
   constructor(private http: HttpClient,private meta:Meta,private titleService: Title ) {
     this.host = window.location.host;
     if ((this.host.toString().indexOf("localhost") + 1) && this.host.toString().indexOf(":")) {
-      this.restItemsUrl = "https://staging.openebs.ci/api/";
+      this.restItemsUrl = "http://localhost:3000";
     } else if (this.host == "openebs.ci") {
         this.restItemsUrl = "https://openebs.ci/api/";
     } else {
