feaf085b2580b5f729406f55653636feaab13958
bhaskarjb <hcbhaskar7@gmail.com>
2018-12-26 20:35:53 +0530

    del unused variable ! Build Success in local

--- src/app/workloads/workloads.component.ts
+++ src/app/workloads/workloads.component.ts
@@ -435,13 +435,13 @@ export class WorkloadsComponent implements OnInit, OnDestroy {
       .val("")
       .change();
   }
-  handleSuccess(captchaResponse: string): void {
+  handleSuccess(): void {
     console.log(this.captchaSuccess);
     this.litmusStarted = true;
     this.captchaSuccess = true;
-    this.captchaResponse = captchaResponse;
     this.captchaIsExpired = false;
     console.log(this.captchaSuccess);
     (<HTMLInputElement> document.getElementById("litmusGo")).style.visibility = "visible";
+    // (<HTMLInputElement> document.getElementById("captchaform")).disabled =true;
   };
 }
