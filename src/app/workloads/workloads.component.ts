1a472ed0c53ce94afaa5c6704ae17d5710c932f7
bhaskarjb <hcbhaskar7@gmail.com>
2018-12-26 15:49:27 +0530

    workload app size reduced in workload dashboard

--- src/app/workloads/workloads.component.ts
+++ src/app/workloads/workloads.component.ts
@@ -233,7 +233,7 @@ export class WorkloadsComponent implements OnInit, OnDestroy {
         //  }
 
           if(this.openebsengine == "cStor"){
-            this.numberReplica = this.numberController*3;
+            this.numberReplica = this.numberController;
           
           }else if(this.openebsengine == "Jiva"){
             this.numberReplica = this.jivaReplicas.length;
