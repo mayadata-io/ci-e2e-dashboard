6cba5f183a8250c446f5dd409a4c0b3cacefaeb3
inyee786 <intakhab.ali@mayadata.io>
2019-02-26 11:51:58 +0530

    update backend api

--- src/app/services/savereaddelete.service.ts
+++ src/app/services/savereaddelete.service.ts
@@ -18,7 +18,7 @@ export class PersonService {
     if ((this.host.toString().indexOf("localhost") + 1) && this.host.toString().indexOf(":")) {
       this.apiurl = "http://localhost:3000/";
     } else {
-      this.apiurl = "https://workloads.openebs.ci/api/";
+      this.apiurl = "https://workloads.openebs.ci/";
     }
   }
 
