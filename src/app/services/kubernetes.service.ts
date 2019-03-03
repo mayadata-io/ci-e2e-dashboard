7829d1b53ef7ae0f8f26619abcbfd9eecc51d80e
inyee786 <intakhab.ali@mayadata.io>
2019-03-03 19:00:17 +0530

    Add functionality for putting all the application
    Signed-off-by: inyee786 <intakhab.ali@mayadata.io>

--- src/app/services/kubernetes.service.ts
+++ src/app/services/kubernetes.service.ts
@@ -1,23 +1,30 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Subject } from "rxjs";
-import { overAllStatus, listNamespace, status } from "../model/data.model";
+import { overAllStatus, listNamespace, status,allApplication } from "../model/data.model";
 
 @Injectable()
 export class KubernetsService {
 
     private apiurl: string;
     private host: string;
+    private gitLabApiUrl: string = 'https://workload-gitlab.openebs.ci/';
+    private workloadApiUrl: string = "https://workloads.openebs.ci/"
     constructor(private http: HttpClient) {
         this.host = window.location.host;
         if ((this.host.toString().indexOf("localhost") + 1) && this.host.toString().indexOf(":")) {
             this.apiurl = "http://localhost:3000/";
-        } else {
-            this.apiurl = "http://workloads.openebs.ci/";
         }
     }
-
+    
+    setApiUrl(apiUrl: string){
+        this.apiurl = apiUrl;
+    }
+    getApiUrl(){
+        return this.apiurl;
+    }
     getJivaVolumeDetails(workloadname:string,openebsengine:string) {
+        console.log(this.apiurl);
         return this.http.get(this.apiurl + "openebs/volume",{
             params:{
                 workloadname:workloadname,
@@ -37,6 +44,7 @@ export class KubernetsService {
     getListNamespaces() {
         return this.http.get<listNamespace>(this.apiurl + "pod/applications")
     }
+
     getAllstatus(namespaces: string) {
         return this.http.get<status>(this.apiurl + "pod/status", {
             params: {
@@ -46,5 +54,12 @@ export class KubernetsService {
     }
     getOpenebsVersion() {
         return this.http.get<listNamespace>(this.apiurl + "pod/openebsversion");
+    }
+
+    getAllApplication(){
+        return this.http.get<allApplication>(this.apiurl + "pod/statuses")
+    }
+    getGitLabApplication(){
+        return this.http.get<allApplication>(this.gitLabApiUrl + "pod/statuses")
     }
 }
