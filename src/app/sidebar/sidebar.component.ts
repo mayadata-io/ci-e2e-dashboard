7c54754a67e6672d462bd8b081488ec96246c36f
bhaskarjb <hcbhaskar7@gmail.com>
2019-03-04 18:45:13 +0530

    del jquery import

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -3,8 +3,6 @@ import { KubernetsService } from "../services/kubernetes.service";
 // import { jiva,cstor } from "../model/data.model";
 import { RouterLinkActive } from '@angular/router';
 import { TranslateModule, TranslateService } from 'angular-intl';
-import * as $ from "jquery";
-
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
