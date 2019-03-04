7695ce6b867d6f58a8b06eb50fce3fd7b3900178
inyee786 <intakhab.ali@mayadata.io>
2019-03-04 18:18:48 +0530

    update image name

--- src/app/services/agile.services.ts
+++ src/app/services/agile.services.ts
@@ -15,7 +15,7 @@ export class AgileService {
   constructor(private http: HttpClient) {
     this.host = window.location.host;
     if (this.host == "localhost:4200") {
-      this.apiurl = "https://localhost:3000/";
+      this.apiurl = "http://localhost:3000/";
     } else if (this.host == "staging.openebs.io") {
       this.apiurl = "https://staging.mayadata.io/api/";
     } else {
