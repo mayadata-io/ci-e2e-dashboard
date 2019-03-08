a7652f9ca77b25a3fe82a261ec4747b735672e13
Chandan Kumar <chandan.kumar@mayadata.io>
2019-03-08 18:13:56 +0530

    Fix the undefined issue in api url
    Signed-off-by: Chandan Kumar <chandan.kumar@mayadata.io>

--- src/app/services/ci-dashboard.service.ts
+++ src/app/services/ci-dashboard.service.ts
@@ -8,15 +8,14 @@ export class DashboardData {
 
     private apiurl: string;
     host: any;
-    restItemsUrl: string;
     constructor(private http: HttpClient, private meta:Meta,private titleService: Title) {
         this.host = window.location.host;
         if ((this.host.toString().indexOf("localhost") + 1) && this.host.toString().indexOf(":")) {
-          this.restItemsUrl = "http://localhost:3000";
+          this.apiurl = "http://localhost:3000";
         } else if (this.host == "openebs.ci" || this.host == "wwww.openebs.ci" ) {
-            this.restItemsUrl = "https://openebs.ci/api/";
+            this.apiurl = "https://openebs.ci/api";
         } else {
-          this.restItemsUrl = "https://staging.openebs.ci/api/";
+          this.apiurl = "https://staging.openebs.ci/api";
         }
     }
 
