19caca230614be457922c93239683ba6fe030056
bhaskarhc <40464957+bhaskarhc@users.noreply.github.com>
2019-04-22 15:36:47 +0530

    Remove inline css and modify the css naming convention (#194)
    Signed-off-by: bhaskarjb <hcbhaskar7@gmail.com>
    
    *  Modify class name according to naming convention

--- src/app/workload-dashboard/workload-dashboard.component.ts
+++ src/app/workload-dashboard/workload-dashboard.component.ts
@@ -2,63 +2,63 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 import { KubernetsService } from "../services/kubernetes.service";
 import * as $ from "jquery";
 import { Subscription, Observable, timer } from "rxjs";
-import { Meta,Title } from "@angular/platform-browser";
+import { Meta, Title } from "@angular/platform-browser";
 import { allApplication } from "../model/data.model";
 import { map, concatMap } from 'rxjs/operators';
 import { ISubscription } from "rxjs/Subscription";
 @Component({
- selector: "app-workload-dashboard",
- templateUrl: "./workload-dashboard.component.html",
- styleUrls: ["./workload-dashboard.component.scss"]
+  selector: "app-workload-dashboard",
+  templateUrl: "./workload-dashboard.component.html",
+  styleUrls: ["./workload-dashboard.component.scss"]
 })
 
 
 export class WorkloadDashboardComponent implements OnInit, OnDestroy {
 
- public openebsVersion : any ;
- public allApplications: allApplication[];
- public responseapp : any = []
- private timerSub: ISubscription;
- public showSpinner: boolean = true;
- constructor(private kubernetsServices: KubernetsService, private meta: Meta,private titleService: Title) {
-   this.titleService.setTitle( "workloads dashboard" );
-   this.meta.updateTag({
-     name: "description",
-     content: "Live Deployments of all the application "
-   });
-  
- }
+  public openebsVersion: any;
+  public allApplications: allApplication[];
+  public responseapp: any = []
+  private timerSub: ISubscription;
+  public showSpinner: boolean = true;
+  constructor(private kubernetsServices: KubernetsService, private meta: Meta, private titleService: Title) {
+    this.titleService.setTitle("workloads dashboard");
+    this.meta.updateTag({
+      name: "description",
+      content: "Live Deployments of all the application "
+    });
 
- ngOnInit() {
-  
-  this.timerSub = timer(0, 3000)
+  }
+
+  ngOnInit() {
+
+    this.timerSub = timer(0, 3000)
       .pipe(concatMap(() => this.kubernetsServices.getAllApplication()))
       .subscribe(res => {
-       const arrayOfApplication = [].concat(...res)
-       if( arrayOfApplication == []){
-        this.showSpinner = true;
-       }else{
-       this.allApplications = arrayOfApplication; 
-       this.showSpinner = false;
-       }
-   });
+        const arrayOfApplication = [].concat(...res)
+        if (arrayOfApplication == []) {
+          this.showSpinner = true;
+        } else {
+          this.allApplications = arrayOfApplication;
+          this.showSpinner = false;
+        }
+      });
 
-   $(document).ready(function(){
-     $("#myInput").on("keyup", function() {
-       var value = $(this).val().toLowerCase();
-       $("#myTable tr").filter(function() {
-         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-       });
-     });
-   });
- }
+    $(document).ready(function () {
+      $("#myInput").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#workloadAppTable tr").filter(function () {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+    });
+  }
 
- setApiUrl(apiurl: string){
-   localStorage.setItem('apiurlkey' , apiurl);
-   this.kubernetsServices.setApiUrl(localStorage.getItem('apiurlkey'));
- }
+  setApiUrl(apiurl: string) {
+    localStorage.setItem('apiurlkey', apiurl);
+    this.kubernetsServices.setApiUrl(localStorage.getItem('apiurlkey'));
+  }
 
- ngOnDestroy() {
-  this.timerSub.unsubscribe();
- }
+  ngOnDestroy() {
+    this.timerSub.unsubscribe();
+  }
 }
