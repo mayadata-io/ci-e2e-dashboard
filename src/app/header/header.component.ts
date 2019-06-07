f7b3a6a8698cfb2d55ed5a42600e9000d10962ae
bhaskarhc <40464957+bhaskarhc@users.noreply.github.com>
2019-06-07 15:48:24 +0530

    fix , rm sidebar icon & build issue (#205)
    Signed-off-by: bhaskarjb <hcbhaskar7@gmail.com>

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -16,20 +16,23 @@ export class HeaderComponent implements OnInit {
   name: FormControl;
   public vendor: any = false;
   email: FormControl;
-  mdape2eURL = 'https://app.mayaonline.io/mdape2e';
   companyname: FormControl;
   isFormEmpty: boolean = false;
   setFormChange: boolean = false;
   mdTag: string = 'OpenEBS OE Newsletter';
+  mdape2e :boolean = false;
   constructor(private router: Router, private headerService: AgileService, public translateService: TranslateService) {
     router.events.subscribe(val => {
       this.currentRoute = this.router.url;
       if (this.currentRoute === "/") {
         this.header = "CI/E2E Dashboard For Master branch of OpenEBS";
+        this.mdape2e = true;
       } else if (this.currentRoute === "/overview") {
         this.header = "Overview";
+        this.mdape2e = false;
       } else {
         this.header = "Workloads Dashboard";
+        this.mdape2e = false;
       }
     });
     this.translateService.getByFileName('VENDOR', 'default-en').subscribe(value => {
