4701f22cc0f2b552db4b2450ebaad2bd5bd0ad89
bhaskarhc <hcbhaskar7@gmail.com>
2021-08-04 17:55:43 +0530

    remove unwanted function call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { TranslateService } from 'angular-intl';
-import { DashboardData } from './services/ci-dashboard.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,21 +8,10 @@ import { DashboardData } from './services/ci-dashboard.service';
 export class AppComponent {
   title = 'dashboard-ci';
   public vendor: any = false;
-  constructor(public translateService: TranslateService, private ApiService: DashboardData,) {
+  constructor(public translateService: TranslateService) {
     this.translateService.getByFileName('VENDOR', 'default-en').subscribe(value => {
       this.vendor = value;
     });
-    console.log('--'.repeat(20) + 'Start' + '---'.repeat(20));
-
-    ApiService.getMayastorTest().subscribe(res => {
-      console.log(res);
-
-    });
-
-    // ApiService.getMayastorTest()
-
-    console.log('--'.repeat(20) + 'END' + '---'.repeat(20));
-
   }
 
   getYear() {
